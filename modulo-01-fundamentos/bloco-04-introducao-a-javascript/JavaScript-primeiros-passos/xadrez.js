380280df463dcad6f6785bebc3341768e38162b7
LiukenMonteiro <liuken37@gmail.com>
2022-09-22 15:16:28 -0300

    mais exercicios

---
+++ modulo-01-fundamentos/bloco-04-introducao-a-javascript/JavaScript-primeiros-passos/xadrez.js
@@ -0,0 +1,19 @@
+let horse = "em l"
+
+
+switch(horse.toLowerCase()) {
+    case 'diagonal':
+      console.log('Movimento errado')
+      break
+    case 'esquerda':
+      console.log('Movimento errado')
+      break
+    case 'direita':
+      console.log('Movimento errado')
+      break
+    case 'em l':
+      console.log('Está certo!!!')
+      break
+    default:
+      console.log('Esse movimento não existe')
+  }
