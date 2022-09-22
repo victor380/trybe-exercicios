3c6d06b21fd6548a728c3c2935a9872c301317c1
LiukenMonteiro <liuken37@gmail.com>
2022-09-22 16:56:26 -0300

    funções array

---
+++ modulo-01-fundamentos/bloco-04-introducao-a-javascript/02-JavaScript-array-e-loop-e-for/array-for-e-loop.js
@@ -0,0 +1,18 @@
+let lotteryNumbers = [34, 43, 23, 56, 28, 6];
+
+// console.log(lotteryNumbers[0]);
+// console.log(lotteryNumbers[1]);
+// console.log(lotteryNumbers[2]);
+// console.log(lotteryNumbers[3]);
+// console.log(lotteryNumbers[4]);
+// console.log(lotteryNumbers[5]);
+
+// console.log(lotteryNumbers.length);
+
+for(let index = 0; index < lotteryNumbers.length; index += 1){
+    // console.log(lotteryNumbers[index]);
+}
+
+for (let index of lotteryNumbers){
+    console.log(index);
+}
