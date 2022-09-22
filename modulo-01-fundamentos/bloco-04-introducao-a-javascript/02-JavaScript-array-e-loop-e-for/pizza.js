3d1f97436ee4fde177fea9dc6679296e804a82b3
LiukenMonteiro <liuken37@gmail.com>
2022-09-22 18:48:00 -0300

    listas

---
+++ modulo-01-fundamentos/bloco-04-introducao-a-javascript/02-JavaScript-array-e-loop-e-for/pizza.js
@@ -0,0 +1,13 @@
+
+
+let pizzas = ['4 Queijos', 'Frango com Catupiry',
+ 'Margeurita', 'Palmito', 'Chocolate']
+
+pizzas.push('Peito de peru')
+pizzas.push('Gratinada')
+
+// console.log(pizzas[0]);
+
+for(let index = 0; index < pizzas.length; index +=1){
+    console.log(pizzas[index]);
+}
