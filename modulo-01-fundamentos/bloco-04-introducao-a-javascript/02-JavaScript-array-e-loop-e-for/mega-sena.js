646adb23a027e5a2b60c5aea5eac95c337b8c6c8
LiukenMonteiro <liuken37@gmail.com>
2022-09-22 17:08:42 -0300

    mega-sena

---
+++ modulo-01-fundamentos/bloco-04-introducao-a-javascript/02-JavaScript-array-e-loop-e-for/mega-sena.js
@@ -0,0 +1,15 @@
+//GERA NÚMERO ALEATÓRIO
+console.log(Math.ceil(Math.random()*60));
+console.log(Math.floor(Math.random()*60));
+
+// console.log(Math.random(10.00001));
+// console.log(Math.random(10.99999));
+//MEGA-SENA
+
+// Sorteio dos números
+
+// Meu jogo
+
+// Comparação do sorteio com o meu jogo
+
+// Quantidade de acertos
