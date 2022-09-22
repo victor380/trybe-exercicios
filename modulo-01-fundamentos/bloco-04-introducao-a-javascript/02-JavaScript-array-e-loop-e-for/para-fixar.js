9994a173d363140d4a231f08d85a01224c17312e
LiukenMonteiro <liuken37@gmail.com>
2022-09-22 19:11:21 -0300

    para fixar

---
+++ modulo-01-fundamentos/bloco-04-introducao-a-javascript/02-JavaScript-array-e-loop-e-for/para-fixar.js
@@ -0,0 +1,16 @@
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+// let menuServices = menu.indexOf('Serviços');
+
+// console.log(menuServices);
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu.indexOf('Portfólio');
+
+// console.log(indexOfPortfolio);
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+menu.push('Contato')
+
+console.log(menu);
